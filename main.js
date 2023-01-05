72eeb09ee65515b45986822e689964311a4b53c1
Murod <mirazizovmurod939@gmail.com>
2023-01-06 01:50:47 +0500

    saved my new site

---
+++ main.js
@@ -0,0 +1,101 @@
+
+
+//console.log('string'); /* matn */
+//console.log(7.7); /* number son  oq rangda chiqadi */
+//console.log(77); /* number son  fioletovi rangda chiqadi */
+//console.log(NaN); /* Not a Number - son emas */
+//console.log(Infinity); /* cheksiz son */
+//console.log(true); /* rost */
+//console.log(false); /* yolg'on */
+//console.log(null); /* bo'sh */
+//console.log(undefined); /* aniqlanmagan */
+
+// O'zgaruvchanla //
+
+ var myName = 'Murod'
+ var myAge = '21'
+ var myJob = 'Programmist'
+
+ console.log(myName);
+ console.log(myAge);
+
+//console.log(' Ismim ' + myName + ' yoshim ' + myAge + ' da ');
+
+ var allinfo = ' ismim ' + myName + ' yoshim ' + myAge + ' da ' + ' kasbim ' + myJob;
+ console.log(allinfo);
+
+//Operatorlar //
+
+// console.log(6+3); //kalkulyatorde xisoplaberadi faqat jovob shaklida ya'ni  9
+// console.log(5-2); //kalkulyatorde xisoplaberadi faqat jovob shaklida ya'ni  3
+// console.log(2*2); //kalkulyatorde xisoplaberadi faqat jovob shaklida ya'ni  4
+// console.log(9%2); //kalkulyatorde xisoplaberadi faqat jovob shaklida ya'ni  1
+
+// console.log(' 6+3 = ' + (6+3));  //misol ko'rinishida xisoplidi
+// console.log(' 5-2 = ' + (5-2));  //misol ko'rinishida xisoplidi
+// console.log(' 2*2 = ' + (2*2));  //misol ko'rinishida xisoplidi
+// console.log(' 9%2 = ' + (9%2));  //misol ko'rinishida xisoplidi
+
+
+// prompt - brauzerda klientdan ma'lumot kiritishini so'raydi
+
+var str = prompt('ismingizni kiriting')
+ alert('Hush kelibsiz ' + str)
+ console.log('Sizning ismingiz ' + str);
+
+//alert ma'lumotni brauzerda alohida oknoda chiqariberadi
+
+var num = +prompt('Misolni yeching: 43 + 57 = ?')
+alert('sizning javobingiz ' + num  + " to'g'ri javob esa: " + (43 + 57))
+console.log('1) 43 + 57 = ' + (43 + 57) + 'Sizning javobingiz: ' + num);
+
+
+
+
+
+
+var str = prompt('ismingizni kiriting')
+ alert('Hush kelibsiz ' + str)
+ console.log('Sizning ismingiz ' + str);
+
+
+var num = +prompt('Misolni yeching: 76 - 21 = ?')
+alert('sizning javobingiz ' + num  + " to'g'ri javob esa: " + (76 - 21))
+console.log('2) 76 - 21 = ' + (76 - 21) + 'Sizning javobingiz:' + num);
+
+
+
+
+
+var str = prompt('ismingizni kiriting')
+ alert('Hush kelibsiz ' + str)
+ console.log('Sizning ismingiz ' + str);
+
+
+var num = +prompt('Misolni yeching: 21 * 17 = ?')
+alert('sizning javobingiz ' + num  + " to'g'ri javob esa: " + (21 * 17))
+console.log('3) 21 * 17 = ' + (21 * 17) + 'Sizning javobingiz:' + num);
+
+
+
+
+
+var str = prompt('ismingizni kiriting')
+ alert('Hush kelibsiz ' + str)
+ console.log('Sizning ismingiz ' + str);
+
+
+var num = +prompt('Misolni yeching: 408 / 34 = ?')
+alert('sizning javobingiz ' + num  + " to'g'ri javob esa: " + (408 / 34))
+console.log('4) 408 / 34 = ' + (408 / 34) + 'Sizning javobingiz:' + num);
+
+
+
+var str = prompt('ismingizni kiriting')
+ alert('Hush kelibsiz ' + str)
+ console.log('Sizning ismingiz ' + str);
+
+
+var num = +prompt('Misolni yeching: 75 % 37 = ?')
+alert('sizning javobingiz ' + num  + " to'g'ri javob esa: " + (75 % 37))
+console.log('5) 75 % 37 = ' + (75 % 37) + 'Sizning javobingiz:' + num);
